adaf41997146705180033ebe29ac5f0d39b7d580
Hasan Ahamed <officials.hasan@gmail.com>
2020-07-27 22:39:16 +0600

    last

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,13 +84,3 @@ $(".btnc").on("click", function () {
 });
 
 
-function printDiv(divName) {
-  var printContents = document.getElementById(divName).innerHTML;
-  var originalContents = document.body.innerHTML;
-
-  document.body.innerHTML = printContents;
-
-  window.print();
-
-  document.body.innerHTML = originalContents;
-}
