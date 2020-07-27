d75bc8c065b4c2f208767619390dc167f8d9e25d
Hasan Ahamed <officials.hasan@gmail.com>
2020-07-27 14:59:28 +0600

    Inital release 1.0

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+var $oBox = $('.outer-box');
+var $gpa = $('#gpa');
+var $credit = $('#credit');
+
+var $result = $('.result').hide();
+
+$('.btnc').click(function() {
+  $('.block').last().clone().children().val("").parent().appendTo($('.inner-box'));
+});
+
+$oBox.on('keyup', '.units', function() {
+  $gpa.text(getTotal());
+});
+$oBox.on('keyup', '.units', function() {
+  $credit.text(getcredit());
+});
+
+
+$oBox.on("change", ".grade-select", function() {
+  $gpa.text(getTotal());
+  $result.is(":hidden") && $result.show();
+});
+$oBox.on("change", ".grade-select", function() {
+  $credit.text(getcredit());
+  $result.is(":hidden") && $result.show();
+});
+
+var points =0;
+var credit = 0;
+$(".cgpa").on("change",function(){
+  points = parseFloat($('.cgpa').val() || 0);
+  $gpa.text(getTotal());
+  $credit.text(getcredit());
+});
+$('.tcredit').on("change", function(){
+  credit = parseFloat($('.tcredit').val() || 0);
+  points = points*credit;
+    $gpa.text(getTotal());
+  $credit.text(getcredit());
+
+})
+
+console.log("points:" + points);
+console.log("credit:" + credit);
+
+
+
+function getTotal() {
+  
+  var credits = 0;
+  var point =0;
+  point = points;
+  credits = credit;
+  
+  $(".units").each(function() {
+    var $this = $(this);
+    if(!isNaN($this.val()) && !isNaN($this.parent().find('.grade-select').val())) {
+      point += parseFloat($this.val() || 0) * parseFloat($this.parent().find('.grade-select').val() || 0);
+      credits += parseFloat($this.val() || 0)
+    }
+  });
+  return  (point/credits).toFixed(2);
+}
+
+function getcredit() {
+  var credits = 0;
+  var point =0;
+  point = points;
+  credits = credit;
+  $(".units").each(function() {
+    var $this = $(this);
+    if(!isNaN($this.val()) && !isNaN($this.parent().find('.grade-select').val())) {
+      point += parseFloat($this.val() || 0) * parseFloat($this.parent().find('.grade-select').val() || 0);
+      credits += parseFloat($this.val() || 0)
+    }
+  });
+  return  (credits);
+}
+
+$(".btnc").on("click", function () {
+  
+    $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+    return false;
+});
