d58dfa00e57322d85cd9d6318fc82c5ed576a0ad
Hasan Ahamed <officials.hasan@gmail.com>
2021-01-06 20:51:10 +0600

    added 2 svg

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -11,13 +11,16 @@ const Footer = () =>
                 <div className="row">
                 
                     <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-                    <h5 className="text-uppercase">Contribute to Opensource</h5>
-                    <p>
-                    Contribute to the most amaizing usefull tools and create a better
-                    community arround us. 
-                    </p>
+                        <h5 className="text-uppercase">Contribute to Opensource</h5>
+                        <p>
+                        Contribute to the most amaizing usefull tools and create a better
+                        community arround us. 
+                        </p>
                     </div>
-           
+                    <div className="col-6">
+                        <img src="assets\pics\undraw_work_together_h63l.svg" alt="" className="img-fluid"/>
+                    </div>
+                    
                 </div>
        
                 </div>
