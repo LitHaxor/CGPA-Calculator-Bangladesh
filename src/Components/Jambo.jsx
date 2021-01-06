9c3a05b42a868a4d66259b5b3fe134951ca657b1
Hasan Ahamed <officials.hasan@gmail.com>
2021-01-06 20:23:30 +0600

    Added SVG: CGPA calculator BD

--- src/Components/Jambo.jsx
+++ src/Components/Jambo.jsx
@@ -4,11 +4,16 @@ const Jambo = (props) => {
         <div>
         <div class="jumbotron jumbotron-fluid content-2">
         <div class="container">
-          <h1 class="display-4"> {props.header} </h1>
-          <p class="lead">{props.message}</p>
           <div className="row">
-          <a  class="btn btn-dark " href="https://github.com/LitHaxor/CGPA-Calculator-Bangladesh">Contribute to GitHub</a>
-          <a href="/compare" class="btn btn-info ">Compare CGPA</a>
+            <div className="col-6">
+              <h1 class="display-4"> {props.header} </h1>
+                <p class="lead">{props.message}</p>
+                <a  class="btn btn-dark " href="https://github.com/LitHaxor/CGPA-Calculator-Bangladesh">Contribute to GitHub</a>
+                <a href="/compare" class="btn btn-info ">Compare CGPA</a>
+            </div>
+            <div className="col-6">
+              <img src="./assets/pics/undraw_percentages_0rur.svg" alt="" height="400px" className="flex-img img-fluid justify-content-center align-items-flex-end"/>          
+            </div>
           </div>
         </div>
       </div>
